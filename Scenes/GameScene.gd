24db7430ee076d3d3b75b8d98c57c59a98cd3e02
MCHuskins <38637043+MCHuskins@users.noreply.github.com>
2022-01-24 13:08:57 -0800

    end of part 5

--- Scenes/GameScene.gd
+++ Scenes/GameScene.gd
@@ -6,7 +6,7 @@ var build_mode = false
 var build_valid = false
 var build_location
 var build_type
-
+var build_tile
 func _ready():
 	map_node = get_node("Map 1")
 
@@ -27,9 +27,12 @@ func _unhandled_input(event):
 	
 
 func initiate_build_mode(tower_type):
+	if build_mode:
+		cancel_build_mode()
 	build_type = tower_type + "t1"
 	build_mode = true
 	get_node("UI").set_tower_preview(build_type, get_global_mouse_position())
+	
 
 func update_tower_preview():
 	var mouse_position = get_global_mouse_position()
@@ -40,6 +43,7 @@ func update_tower_preview():
 		get_node("UI").update_tower_preview(tile_position,"ad54ff3c")
 		build_valid = true
 		build_location = tile_position
+		build_tile = current_tile
 	else:
 		get_node("UI").update_tower_preview(tile_position,"adff4545")
 		build_valid = false
@@ -48,10 +52,12 @@ func update_tower_preview():
 func cancel_build_mode():
 	build_mode = false
 	build_valid = false
-	get_node("UI/TowerPreview").queue_free()
+	get_node("UI/TowerPreview").free()
 
 func verify_and_build():
 	if build_valid:
 		var new_tower = load("res://Rescources/fans/" + build_type + ".tscn").instance()
 		new_tower.position = build_location
 		map_node.get_node("Towers").add_child(new_tower,true)
+		map_node.get_node("notower").set_cellv(build_tile,5)
+		
---
+++ Singletons/gamedata.gd
@@ -0,0 +1,10 @@
+extends Node
+var tower_data = {"fant1" : {
+	"damage" : 20,
+	"ref" : 1, 
+	"range" : 350}, 
+	"mist1" : {
+		"damage" : 100,
+		"ref" : 4, 
+		"range" : 555}
+	}
