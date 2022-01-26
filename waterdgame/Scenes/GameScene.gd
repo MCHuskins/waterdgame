031007534a349f32e8b63f71ef2d57da53e20cae
MCHuskins <38637043+MCHuskins@users.noreply.github.com>
2022-01-26 13:11:36 -0800

    s

--- waterdgame/Scenes/GameScene.gd
+++ waterdgame/Scenes/GameScene.gd
@@ -34,11 +34,14 @@ func _unhandled_input(event):
 
 func start_next_wave():
 	var wave_deta = retrieve_wave_deta()
-	yield(get_tree().create_timer(0.2),"timeout")
+	yield(get_tree().create_timer(0.0),"timeout")
 	spawn_enemies(wave_deta)
 	
 func retrieve_wave_deta():
-	var wave_delta = [["buletank", 3.7], ["buletank",0.1]]
+	var wave_delta = [["buletank", 1.7], ["buletank",0.1], ["buletank", 2.7], ["buletank",0.1]]
+	for i in range(current_wave):
+		wave_delta.append(["buletank", 1.7])
+		wave_delta.append(["buletank",0.1])
 	current_wave += 1
 	enemies_in_wave = wave_delta.size()
 	return wave_delta
@@ -48,6 +51,9 @@ func spawn_enemies(wave_delta):
 		var new_enemy = load("res://Rescources/enemies/" + i[0] + ".tscn").instance()
 		map_node.get_node("Path").add_child(new_enemy, true)
 		yield(get_tree().create_timer(i[1]),"timeout")
+	yield(get_tree().create_timer(1.0),"timeout")
+	start_next_wave()
+
 
 ##
 ## build thigns
